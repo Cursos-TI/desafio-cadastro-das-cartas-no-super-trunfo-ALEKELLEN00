cadd14aec0e28e39e55bb6c005aba4cfc5a7d8f7
ALEKELLEN00 <alexandrakellen7@gmail.com>
2025-04-21 23:52:01 +0000

    nivel metre

--- DesafioSuperTrunfo.c
+++ DesafioSuperTrunfo.c
@@ -6,16 +6,22 @@ int main (){
     printf("\n");
 
     // Declaração de variáveis da cidade 1
-    int populacao;
+    unsigned long int populacao;
     float area;
     float pib;
     int numero_pontos_turisticos;
+    float densidade_populacional;
+    float pib_percapita;
+    float super_poder;
 
     // Declaração de variáveis da cidade 2
-    int populacao2;
+    unsigned long int populacao2;
     float area2;
     float pib2;
     int numero_pontos_turisticos2;
+    float densidade_populacional2;
+    float pib_percapita2;
+    float super_poder2;
 
     // Onde o usuário informa a população da cidade 1
     printf("Digite a população cidade 1: ");
@@ -57,6 +63,21 @@ int main (){
     // Onde o programa está anexando o valor inserido pelo usuário em sua respectiva variavel
     scanf("%d", &numero_pontos_turisticos2);
 
+    //Onde esta armazenando os valores de densidade, pib per capita e super poder cidade 1.
+    densidade_populacional = populacao/area;
+    pib_percapita = pib/populacao;
+    super_poder = 1/densidade_populacional + (populacao + area + pib + numero_pontos_turisticos + pib_percapita + super_poder);
+
+
+
+    //Onde esta armazenando os valores de densidade, pib per capita e super poder cidade 2.
+    densidade_populacional2 = populacao2/area2;
+    pib_percapita2 = pib2/populacao2;
+    super_poder = 1/densidade_populacional2 + (populacao2 + area2 + pib2 + numero_pontos_turisticos2 + pib_percapita2 + super_poder2 );
+    
+
+
+
     //Bloco de printfs que informa os valores da cidade 1
     printf("\n");
     printf("Valores informados cidade 1:\n");
@@ -65,8 +86,10 @@ int main (){
     printf("A area é de %.2f \n", area);
     printf("O pib é de %.2f \n", pib);
     printf("Numero de pontos turisticos é de %d \n", numero_pontos_turisticos);
-    printf ("Densidade populacional é de %.2f \n", populacao/area);
-    printf ("Pib per capita é de %.2f \n", pib/populacao);
+    printf ("Densidade populacional é de %.2f \n", densidade_populacional);
+    printf ("Pib per capita é de %.2f \n", pib_percapita);
+
+
     
     //Bloco de printfs que informa os valores da cidade 2
     printf("\n");
@@ -76,8 +99,12 @@ int main (){
     printf("A area é de %.2f\n", area2);
     printf("O pib é de %.2f\n", pib2);
     printf("Numero de pontos turisticos é de %d\n", numero_pontos_turisticos2);
-    printf ("Densidade populacional é de %.2f \n", populacao2/area2);
-    printf ("Pib per capita é de %.2f \n", pib2/populacao2);
+    printf ("Densidade populacional é de %.2f \n", densidade_populacional2);
+    printf ("Pib per capita é de %.2f \n", pib_percapita2);
+
+    //Comparaçao entre as cidades 
+    unsigned long int compPop = populacao > populacao2;
+    printf(compPop);
 
 return 0;
 }
